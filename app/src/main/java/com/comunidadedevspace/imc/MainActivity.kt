8d4ad86166990bff25c8886743b035a37fa5e0b7
Ivar05 <ivar.araujorecife@gmail.com>
2024-04-21 18:48:06 -0400

    Tela inicial

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,31 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // recuper componentes edittext
+        // criar uma variavel e associar o component de UI
+        // recuperar o botao de tela
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+        val peso = edtPeso.text
+        val altura = edtAltura.text
+        println("ivar acao botao " + altura)
+
+
+
+
+        }
     }
 }
